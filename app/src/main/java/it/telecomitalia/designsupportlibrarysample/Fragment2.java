94020c80462840b7289ba8c6ea04ff130898c3a0
Octavian Ionel <octavianmarcel.ionel@guest.telecomitalia.it>
2015-07-29 10:06:52 +0200

    TabLayout & ViewPager functioning correctly

--- app/src/main/java/it/telecomitalia/designsupportlibrarysample/Fragment2.java
+++ app/src/main/java/it/telecomitalia/designsupportlibrarysample/Fragment2.java
@@ -1,9 +1,23 @@
 package it.telecomitalia.designsupportlibrarysample;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by octavian on 7/24/15.
  */
 public class Fragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView tv =new TextView(container.getContext());
+        tv.setText("Fragment 2");
+        return tv;
+    }
+
 }
--- app/src/main/java/it/telecomitalia/designsupportlibrarysample/Fragment3.java
+++ app/src/main/java/it/telecomitalia/designsupportlibrarysample/Fragment3.java
@@ -1,9 +1,22 @@
 package it.telecomitalia.designsupportlibrarysample;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by octavian on 7/24/15.
  */
 public class Fragment3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView tv =new TextView(container.getContext());
+        tv.setText("Fragment 3");
+        return tv;
+    }
 }
